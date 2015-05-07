80f1434bcde74fbc3c49139313dd2fa7138a07a0
Ryan Dodds <rydodds@gmail.com>
2015-05-07 16:19:30 -0700

    A bunch of shit for registration and user roles

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -83,3 +84,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com' 
+EMAIL_HOST_USER = '' 
+EMAIL_HOST_PASSWORD = '' 
+EMAIL_USE_TLS = False 
+EMAIL_PORT = 1025
+
+LOGIN_REDIRECT_URL = "home"
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,19 +1,35 @@
+from collection.backends import MyRegistrationView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', 'collection.views.index', name='home'),
-    url(r'^about/$',
-		TemplateView.as_view(template_name='about.html'),
-		name='about'), 
-    url(r'^contact/$',
-		TemplateView.as_view(template_name='contact.html'),
-		name='contact'),
-    url(r'^interviews/(?P<slug>[-\w]+)/$', 
-    	'collection.views.interview_detail', 
-    	name='interview_detail'),
-    url(r'^interviews/(?P<slug>[-\w]+)/edit/$', 
-    	'collection.views.edit_interview', name='edit_interview'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'), 
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    
+    url(r'^interviews/(?P<slug>[-\w]+)/$', 'collection.views.interview_detail', name='interview_detail'),
+    url(r'^interviews/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_interview', name='edit_interview'),
+
+    url(r'^accounts/password/reset/$', password_reset, 
+        {'template_name': 'registration/password_reset_form.html'}, 
+        name="password_reset"),
+    url(r'^accounts/password/reset/done/$', 
+        password_reset_done, 
+        {'template_name': 'registration/password_reset_done.html'}, 
+        name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        password_reset_confirm, 
+        {'template_name': 'registration/password_reset_confirm.html'}, 
+        name="password_reset_confirm"),
+    url(r'^accounts/password/done/$', password_reset_complete,
+        {'template_name': 'registration/password_reset_complete.html'},
+        name="password_reset_complete"),
+
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/create_interview/$', 'collection.views.create_interview', name='registration_create_interview'),
+
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
