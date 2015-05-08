3ad55c9717530b7de7faa24ca1070199487fa665
Ryan Dodds <rydodds@gmail.com>
2015-05-08 10:27:52 -0700

    wsgi file update

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os 
-	os.environ.setdefault(“DJANGO_SETTINGS_MODULE”,
-		“hellowebapp.settings_production”)
+os.environ.setdefault(“DJANGO_SETTINGS_MODULE”,
+	“hellowebapp.settings_production”)
 
 from django.core.wsgi import get_wsgi_application 
 from whitenoise.django import DjangoWhiteNoise
