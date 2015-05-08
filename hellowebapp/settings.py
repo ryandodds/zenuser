e34f4549ecb3fc00c64902cdfbd47dfb070966e5
Ryan Dodds <rydodds@gmail.com>
2015-05-07 21:37:30 -0700

    Added bootstrap

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'registration',
+    'twitter_bootstrap',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,6 +83,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 STATIC_URL = '/static/'
 
