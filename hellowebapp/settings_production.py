e357d832af95d553075cc57f9d11e6ece457285e
Ryan Dodds <rydodds@gmail.com>
2015-05-08 10:45:52 -0700

    changes to settings_production

--- hellowebapp/settings_production.py
+++ hellowebapp/settings_production.py
@@ -1,10 +1,11 @@
-# Inherit from standard settings file for defaults
+# Inherit from standard settings file for default
 from hellowebapp.settings import *
 
 # Everything below will override our standard settings:
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -13,14 +14,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Set debug to False
-DEBUG = False 
+DEBUG = False
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../collection/static'),
-)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
