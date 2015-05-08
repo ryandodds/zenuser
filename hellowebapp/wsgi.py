5541fbfce5963a89340444e3395882d553ff735e
Ryan Dodds <rydodds@gmail.com>
2015-05-08 10:34:53 -0700

    more changes

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -1,18 +1,7 @@
-"""
-WSGI config for hellowebapp project.
+import os
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
-import os 
-os.environ.setdefault(“DJANGO_SETTINGS_MODULE”,
-	“hellowebapp.settings_production”)
-
-from django.core.wsgi import get_wsgi_application 
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application() 
-application = DjangoWhiteNoise(application)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings_production")
+application = Cling(get_wsgi_application())
