c2add8b2f5fc0e0d78cd312946b553b4406e7a70
Ryan Dodds <rydodds@gmail.com>
2015-05-08 10:42:20 -0700

    more changes

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -1,7 +1,9 @@
-import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+import os 
+os.environ.setdefault(“DJANGO_SETTINGS_MODULE”,
+	“hellowebapp.settings_production”)
 
+from django.core.wsgi import get_wsgi_application 
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings_production")
-application = Cling(get_wsgi_application())
+application = get_wsgi_application() 
+application = DjangoWhiteNoise(application)
