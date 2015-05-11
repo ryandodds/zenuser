bef6457c1288d78c32571549da3da71f9779a198
Ryan Dodds <rydodds@gmail.com>
2015-05-11 16:25:01 -0700

    more style updates and add interview button

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -1,7 +1,5 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings")
+
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings_production")
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
